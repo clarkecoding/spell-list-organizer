8ff8c632d9dfd281cbd59733637ef0bcf646ff0c
Sarah Clarke <sarclarke4@gmail.com>
2023-07-06 16:59:43 -0300

    Fix code formatting in App.js Incorrect

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,15 @@ const testSpells = require('../data/test_spells.json');
 function App() {
   return (
     <div className='App'>
-    <SpellWidget 
-      spell={testSpells.spells[0]}
-    />
-    <SpellWidget 
-      spell={testSpells.spells[1]}
-    />
-    <SpellWidget 
-      spell={testSpells.spells[2]}
-    />
+      <SpellWidget 
+        spell={testSpells.spells[0]}
+      />
+      <SpellWidget 
+        spell={testSpells.spells[1]}
+      />
+      <SpellWidget 
+        spell={testSpells.spells[2]}
+      />
     </div>
   );
 }
