0cbbb0b950888dcd7e69737bb3921088ad766cc8
Sarah Clarke <sarclarke4@gmail.com>
2023-07-15 02:32:01 -0300

    Alter formatting of SpellWidgets Spell components now only display "VSM" and are comma separated. Concentration is now indicated in the spell's Duration section. Ritual casting is now indicated in the spell's Casting Time section.

--- src/components/SpellWidget.js
+++ src/components/SpellWidget.js
@@ -2,21 +2,40 @@ import { useState } from 'react';
 import '../SpellWidget.css';
 
 function SpellWidget(props) {
-
   let upcastText;
-  if (props.spell.upcast_description === "N/A")
-    upcastText = "";
-  else
+  if (props.spell.hasOwnProperty('upcast_description'))
     upcastText = (
       <div><br /> <strong>At Higher Levels: </strong><br />{ props.spell.upcast_description }</div>
-    )
+    );
+  else
+    upcastText = "";
 
   let spellLevel;
   if (props.spell.level == 0)
-      spellLevel = "Cantrip";
+    spellLevel = "Cantrip";
   else
-      spellLevel = `Level ${props.spell.level}`;
+    spellLevel = `Level ${props.spell.level}`;
+  
+  let duration;
+  if (props.spell.components.charAt(3) ==='C')
+    duration = <td><i>Concentration, up to</i><br/>{props.spell.duration}</td>;
+  else
+    duration = <td> {props.spell.duration} </td>;
 
+  let castingTime;
+  if (props.spell.components.charAt(4) ==='R')
+    castingTime = <td> {props.spell.casting_time} <br /><i>or cast as ritual</i></td>;
+  else
+    castingTime = <td> {props.spell.casting_time} </td>;
+  
+  let components = "";
+  for (let i = 0; i < 3; i++) {
+    const c = props.spell.components.charAt(i);
+    if (c !== 'x')
+      components += c + ", ";
+  }
+  components = <td>{components.substr(0, components.length - 2)}</td>;
+  
   return (
     <details className='spell-widget'>
       <summary className='flex-container-horizontal spell-widget-header'>
@@ -25,7 +44,7 @@ function SpellWidget(props) {
         <span className='flex-grow-0 spell-widget-source'>Source: { props.spell.source }</span>
       </summary>
       <div className='flex-container-horizontal'>
-        <div className='flex-grow-0'>
+        <div className='spellInfoTable flex-grow-0'>
           <table>
             <tr>
               <td>School:</td>
@@ -33,7 +52,7 @@ function SpellWidget(props) {
             </tr>
             <tr>
               <td>Casting Time:</td>
-              <td>{ props.spell.casting_time }</td>
+              { castingTime }
             </tr>
             <tr>
               <td>Range:</td>
@@ -41,11 +60,11 @@ function SpellWidget(props) {
             </tr>
             <tr>
               <td>Components:</td>
-              <td>{ props.spell.components }</td>
+              {components}
             </tr>
             <tr>
               <td>Duration:</td>
-              <td>{ props.spell.duration }</td>
+              {duration}
             </tr>
           </table>
         </div>
